3dd25dcd4fcf9974552e8b875d7c2356b0d054d8
snehal <snehal@idyllic.co>
2018-04-24 23:22:29 -0400

    changes in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 10.times do |proposal|
-	Proposal.create!(customer: "#{proposal}",
+	Proposal.create!(customer: "Customer #{proposal}",
 	                 portfolio_url: 'http://google.com',
 	                 tools: "Ruby on Rails, Angular",
 	                 estimated_hours: (80 + proposal),
